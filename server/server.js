1b133a915eaaf4eb9c9ff90a2fe88edeed6562e2
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-26 19:47:38 +0400

    create server

--- server/server.js
+++ server/server.js
@@ -13,18 +13,40 @@ const productSchema = new mongoose.Schema({
   position: String,
 });
 
+mongoose.set("strictQuery", false);
+app.use(express.json());
+app.use(cors());
+mongoose
+  .connect(
+    `mongodb+srv://ferid:PSZde9zf0vkXCbWc@cluster0.tnvvtt5.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.error(err));
+
 const ProductModel = mongoose.model("Product", productSchema);
 
-app.get("api/products", (req, res) => {
-  const products = ProductModel.find();
+app.get("/api/products", async (req, res) => {
+  const products = await ProductModel.find();
   res.send(products);
 });
 
-app.get("api/products", (req, res) => {
-  const products = ProductModel.find();
-  res.send(products);
+app.post("/api/products", async (req, res) => {
+  const data = req.body;
+  const addData = new ProductModel(data);
+  await addData.save();
+  res.send(addData);
 });
 
-app.listen(() => {
+app.delete("/api/products/:id", (req, res) => {
+  if (req.params.id)
+    ProductModel.findByIdAndDelete(req.params.id, (error, data) => {
+      if (error) {
+        return res.status(500).send({ error });
+      }
+      res.send(data);
+    });
+});
+
+app.listen(PORT, () => {
   console.log(`listen start port ${PORT}`);
 });
