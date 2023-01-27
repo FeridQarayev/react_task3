bffb46fb72303f341e0d7f41897058862ee08cd5
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-27 17:48:52 +0400

    update server  && add joi

--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
-const moongose = require("mongoose");
-const cors = require("cors");
+const mongoose = require("mongoose");
 const express = require("express");
-const { default: mongoose } = require("mongoose");
+const cors = require("cors");
+const Joi = require("joi");
 
 const PORT = 8080;
 
 const app = express();
 
-const productSchema = new mongoose.Schema({
+const ProductSchema = new mongoose.Schema({
   name: String,
   description: String,
-  position: String,
+  price: String,
 });
+
+const ProductModel = mongoose.model("Product", ProductSchema);
 
 mongoose.set("strictQuery", false);
 app.use(express.json());
 app.use(cors());
+
 mongoose
   .connect(
     `mongodb+srv://ferid:PSZde9zf0vkXCbWc@cluster0.tnvvtt5.mongodb.net/?retryWrites=true&w=majority`
@@ -23,28 +26,62 @@ mongoose
   .then(() => console.log("Connected to database"))
   .catch((err) => console.error(err));
 
-const ProductModel = mongoose.model("Product", productSchema);
-
-app.get("/api/products", async (req, res) => {
-  const products = await ProductModel.find();
-  res.send(products);
+const productValSchema = Joi.object({
+  name: Joi.string().required(),
+  description: Joi.string().required(),
+  price: Joi.number().required(),
 });
 
-app.post("/api/products", async (req, res) => {
-  const data = req.body;
-  const addData = new ProductModel(data);
-  await addData.save();
-  res.send(addData);
+// Get All Products
+app.get("/api/products", (req, res) => {
+  ProductModel.find().exec((error, data) => {
+    if (error) return res.status(500).send({ error });
+    res.send(data);
+  });
 });
 
+// Get By id product
+app.get("/api/products/:id", (req, res) => {
+  if (req.params.id) {
+    ProductModel.findById(req.params.id, (error, data) => {
+      if (error) return res.status(500).send({ error });
+      res.send(data);
+    });
+  } else return res.status(400).send({ error: "Id not found" });
+});
+
+// Create Product
+app.post(
+  "/api/products",
+  (req, res, next) => {
+    const { error } = productValSchema.validate(req.body);
+
+    if (error == null) next();
+    else {
+      const { details } = error;
+      const message = details.map((i) => i.message).join(",");
+      res.status(422).json({ error: message });
+    }
+  },
+  async (req, res) => {
+    const newProduct = new ProductModel({ ...req.body });
+    await newProduct.save();
+    res
+      .status(201)
+      .send({ message: "Product succesfully added!", product: newProduct });
+  }
+);
+
+//Delete Product
 app.delete("/api/products/:id", (req, res) => {
-  if (req.params.id)
+  if (req.params.id) {
     ProductModel.findByIdAndDelete(req.params.id, (error, data) => {
       if (error) {
         return res.status(500).send({ error });
       }
       res.send(data);
     });
+  } else return res.status(400).send({ error: "Id not found" });
 });
 
 app.listen(PORT, () => {
